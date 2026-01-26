447897917dbac086a0580547ad2b3120afc2ee3d
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2026-01-26 23:53:34 +0530

    feat: Add helmet middleware for enhanced security and disable x-powered-by header

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express  = require("express");
 const app  = express();
 const apiRouter = require("./routers");
 const { serverConfig } = require("./config");
+
+
+app.disable("x-powered-by");
 app.use(express.json());
 app.use(express.urlencoded());
 

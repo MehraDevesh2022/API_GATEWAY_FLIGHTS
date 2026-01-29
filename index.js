4adf16ba81b63992576b25300404890639c3d49c
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2026-01-29 23:57:21 +0530

    feat: Integrate helmet middleware for security enhancements

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express  = require("express");
 const app  = express();
 const apiRouter = require("./routers");
 const { serverConfig } = require("./config");
-
+const helmet = require("helmet");
 
 app.disable("x-powered-by");
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
 
